773e241c831a6768c880d7bd4da36075e84a6ca0
Tobias Lidskog <tobias@lidskog.se>
2015-07-09 17:11:45 +0200

    Switch vars to camelCase. Just for you eslint!

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 var cp = require('child_process'),
     semver = require('semver'),
     chromedriver = require('./'),
-    package_version = require('./package.json').version;
+    packageVersion = require('./package.json').version;
 
-var expected_version_prefix = 'ChromeDriver ' + semver.major(package_version) + '.' + semver.minor(package_version);
+var expectedVersionPrefix = 'ChromeDriver ' + semver.major(packageVersion) + '.' + semver.minor(packageVersion);
 
-var driver_version = cp.execFileSync(chromedriver.binPath(), ['--version']).toString();
+var driverVersion = cp.execFileSync(chromedriver.binPath(), ['--version']).toString();
 
-if (driver_version.indexOf(expected_version_prefix) !== 0) {
-  throw new Error('Expected driver version to be ' + expected_version_prefix + ' but it was ' + driver_version);
+if (driverVersion.indexOf(expectedVersionPrefix) !== 0) {
+  throw new Error('Expected driver version to be ' + expectedVersionPrefix + ' but it was ' + driverVersion);
 }
