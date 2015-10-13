ca7a174184d3d05a7209f792d80ab091eebe2ceb
Tobias Lidskog <tobias@lidskog.se>
2015-10-13 15:51:41 +0200

    Wrap exports in chromedriver object.

--- index.js
+++ index.js
@@ -2,14 +2,15 @@
 
 var os = require('os'),
   path = require('path'),
-  version = require('./package').version;
+  pkg = require('./package');
 
-module.exports.version = version;
-
-module.exports.binPath = function() {
-  var driverPath = path.resolve(__dirname, 'vendor', 'chromedriver');
-  if (os.platform() === 'win32') {
-    driverPath = driverPath + '.exe';
+module.exports.chromedriver = {
+  version: pkg.version,
+  binPath: function() {
+    var driverPath = path.resolve(__dirname, 'vendor', 'chromedriver');
+    if (os.platform() === 'win32') {
+      driverPath = driverPath + '.exe';
+    }
+    return driverPath;
   }
-  return driverPath;
 };
