1bb421033e1ab747c4c85c617df02927d3e00936
soulgalore <peter@soulgalore.com>
2017-09-20 09:27:54 +0200

    2.32

--- install.js
+++ install.js
@@ -9,7 +9,7 @@ function getChromedriverUrl() {
   if (process.env.CHROMEDRIVER_BASE_URL) {
     urlBase = process.env.CHROMEDRIVER_BASE_URL;
   } else {
-    urlBase = 'https://chromedriver.storage.googleapis.com/2.31/';
+    urlBase = 'https://chromedriver.storage.googleapis.com/2.32/';
   }
 
   switch (os.platform()) {
