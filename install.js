e9009b953e3d31625a9f308864cc2eb36dbf66ca
Peter Hedenskog <phedenskog@wikimedia.org>
2018-10-17 11:55:13 +0200

    2.43.0

--- install.js
+++ install.js
@@ -9,7 +9,7 @@ function getChromedriverUrl() {
   if (process.env.CHROMEDRIVER_BASE_URL) {
     urlBase = process.env.CHROMEDRIVER_BASE_URL;
   } else {
-    urlBase = 'https://chromedriver.storage.googleapis.com/2.42/';
+    urlBase = 'https://chromedriver.storage.googleapis.com/2.43/';
   }
 
   switch (os.platform()) {
